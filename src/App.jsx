f2e4dc808d56992fe7c0279dfee0d7827cd044dd
Shubham Mote <moteshubham@gmail.com>
2023-11-13 15:33:31 +0530

    init meme-generator project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "../components/Header";
+import Meme from "../components/Meme";
+import React from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+  );
+}
+
+export default App;
