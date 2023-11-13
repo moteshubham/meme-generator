f1375d31cac9862705d90845ac7430ee7a3caa6a
Shubham Mote <moteshubham@gmail.com>
2023-11-13 16:57:42 +0530

    ima

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="../src/assets/troll.png" 
+                src="./src/assets/troll.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
